fa57e17e1abb0a5072c13512c78d41bbb961a653
Mart <pobtiny@aol.com>
2025-07-13 13:32:46 +0100

    new

--- crash_course/for.py
+++ crash_course/for.py
@@ -7,3 +7,16 @@ for pizza in pizzas:
     x = len(pizzas)
     print("there are " + str (x) + " pizzas on the menu.")
 
+digits = [1,2,3,4,5,6,7,8,9,0]
+
+print(min(digits))
+print(max(digits))
+print(sum(digits))
+
+list = []
+
+for num in range(1, 11):
+    list.append(num)
+print(list)
+
+
